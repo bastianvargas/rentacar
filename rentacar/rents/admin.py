af860833ad5cebac2300bec770f1c45499aed635
bastianvargas <bastianlc@live.cl>
2021-07-20 23:36:28 -0400

    registered models in admin site

--- rentacar/rents/admin.py
+++ rentacar/rents/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Rent
+
+
+class RentAdmin(admin.ModelAdmin):
+
+    list_display = ('company', 'client', 'daily_cost', 'days') 
+
+
+admin.site.register(Rent, RentAdmin)
