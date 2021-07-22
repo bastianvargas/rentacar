22c4dcff34c141ec2ee2dc1baa7ddb267acd8de9
bastianvargas <bastianlc@live.cl>
2021-07-21 22:38:37 -0400

    add related_name in foreign keys

--- rentacar/rents/models.py
+++ rentacar/rents/models.py
@@ -6,8 +6,8 @@ from companys.models import Company
 # Create your models here.
 
 class Rent(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='client_rents')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_rents')
     daily_cost = models.IntegerField()
     days = models.IntegerField()
 
